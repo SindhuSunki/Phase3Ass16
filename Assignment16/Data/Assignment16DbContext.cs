ab420824ffb89cbe0e72ed07389a09ef02c8b72b
sindhusunki789@gmail.com <sindhusunki789@gmail.com>
2023-09-03 22:26:11 +0530

    first commit

---
+++ Assignment16/Data/Assignment16DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment16.Models;
+
+namespace Assignment16.Data
+{
+    public class Assignment16DbContext : DbContext
+    {
+        public Assignment16DbContext (DbContextOptions<Assignment16DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment16.Models.Task> Task { get; set; } = default!;
+    }
+}
